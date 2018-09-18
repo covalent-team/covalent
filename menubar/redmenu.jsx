ba66be4eaa1a235f84615e98d545c93b4b78ed9f
Cassandra Le <lenguyetminh1998@gmail.com>
2018-09-18 16:50:22 -0400

    Finish building backbone of file structure

--- menubar/redmenu.jsx
+++ menubar/redmenu.jsx
@@ -1,67 +1,132 @@
 const fs = require('fs'); 
 
+// This renders the file structure folder on the app 
 class RedMenu extends React.Component{
-    constructor(props){
-        super(props); 
-        this.renderedDiv; 
-      
-        // Explorer Components 
-        this.uploadInput =  <input type="file" onChange={ (e) => this.handleChange(e.target.files) }></input> 
-        this.state = {
-            fileNames: [] 
-        }
-    }
+  constructor(props){
+		super(props); 
+		this.renderedDiv; 
+		
+		// Explorer Components 
+		this.uploadInput =  <input type="file" onChange={ (e) => this.handleChange(e.target.files) }></input> 
+		this.state = {
+				fileNames: [], 
+				menuPath: "", 
+				stateTree: [] 
+		}
+	}
 
-    // Get a list of files when user click add workspace 
-    handleChange(selectorFiles){
-        var name = selectorFiles[0].name;
-        var path = selectorFiles[0].path.replace(name, ''); 
-        var localFile = []; 
-        fs.readdir(path, (err, files) => { 
-            localFile.push(files); 
-            this.setState({
-                fileNames: localFile 
-            });   
-        }); 
-      
-    } 
+  // Handle workspace creation when user click add file button 
+	handleChange(selectorFiles){
+		var path = selectorFiles[0].path; 
+		var fileName = selectorFiles[0].name; 
+		var treePath = path.replace(fileName, '');  
+		var getTreeContentPromise = this.getTreeContent(treePath); 
+		getTreeContentPromise.then((treeContent) => {
+			this.setState({
+				fileNames: treeContent, 
+				menuPath: treePath 
+			})
+			this.buildTree();  
+		});  
+	} 
+		
+	/* Return an array of content within a path for the tree structure  
+		 @return: [file1.png, file2.png...etc.]
+	*/ 
+	getTreeContent(treePath){
+		var localFile = []; 
+		return new Promise((resolve, reject) => {
+			fs.readdir(treePath, (err, files) => {
+				if (err){
+					return console.log(err); 
+				} else{
+					localFile.push(files); 
+					resolve(localFile); 
+				}
+			})
+		})
+	}
 
-    checkDirectory(file){
-        fs.lstat(path, (err, stats) => {
-            console.log(stats)
-            console.log("lstat running"); 
-            if(err) return console.log(err); 
-            else{
-                console.log(`Is file: ${stats.isFile()}`); 
-                console.log(`Is directory: ${stats.isDirectory()}`); 
-            }
-        })
-    }
+	// Check if a path is directory or a file 
+	checkDirectory(path){
+		return new Promise((resolve, reject) => {
+			fs.lstat(path, (err, stats) => {
+				if (err) {
+					return console.log(err); 
+				} else {
+					if (stats.isFile() == true){
+						resolve(false); 
+					} else {
+						resolve(true); 
+					}
+				}
+			})  
+		}); 
+	}
 
-    // Return a list of files if user had added workspace 
-    render(){
-        console.log("this is the filenames", this.state.fileNames); 
-        const folderTree = []; 
-        if (this.state.fileNames.length > 0){
-            for (var f in this.state.fileNames[0]){
-                var fileHTML = <p key={f}>{this.state.fileNames[0][f]} </p>;  
 
-                folderTree.push(fileHTML); 
-            }
-            return (
-                <div>
-                    <h4>Workspace</h4> 
-                    {folderTree}
-                </div>
-            ); 
-        }
-        return ( 
-            <div>
-                <h4>Workspace</h4> 
-                {this.uploadInput}
-            </div>
-        )
-    }
+	buildTree(){
+		console.log("this state filenames", this.state.fileNames); 
+		var renderedTree = [];   
+		var checkDirectoryPromiseArr = []; 
+		
+
+		// Return an array if a file is a file, or a directory 
+		if (this.state.fileNames.length > 0){
+			for (var f in this.state.fileNames[0]){
+				var filePath = this.state.menuPath + this.state.fileNames[0][f]; 
+				var checkDirectoryPromise = this.checkDirectory(filePath); 
+				checkDirectoryPromiseArr.push(checkDirectoryPromise); 
+			}
+
+			// Create the icons to embedded within the p tag 
+			var folderIcon = <i className="fas fa-folder-open"></i>; 
+			var fileIcon = <i className="fas fa-file"></i>
+			var folderLine = <p>{folderIcon}Hello World</p>; 
+		
+			// Create the <p> tag for each file and folder icon and add it to the rendered tree 
+			Promise.all(checkDirectoryPromiseArr).then((checkDirectoryArrVal) => {
+				checkDirectoryArrVal.forEach((val, i) => {
+					if (val == true){
+						var folderLine = <p key={i}> {folderIcon}{this.state.fileNames[0][i]}</p>
+						renderedTree.push(folderLine); 
+					} 
+					else{
+						var fileLine = <p key={i}> {fileIcon}{this.state.fileNames[0][i]}</p> 
+						renderedTree.push(fileLine); 
+					}
+				})
+				this.setState({
+					stateTree: renderedTree
+				}); 
+			})
+		}
+	}
+
+	
+
+  // Return a list of files if user had added workspace 
+	render(){
+		if (this.state.stateTree.length > 0){
+			console.log("statetree", this.state.stateTree);  
+			return (
+				<div>
+					<h4>Workspace</h4>
+					{this.state.stateTree}
+				</div>
+			)
+		}
+		
+
+
+		// Else return input box for user to chooose file 
+		return ( 
+				<div>
+						<h4>Workspace</h4> 
+						{this.uploadInput}
+				</div>
+		)
+	}
 }
 
 // Render the black menu from react. 
